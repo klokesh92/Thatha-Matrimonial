4c42e0e1def393941531a37e18ce2b7c124e499b
klokesh92 <lokeshgdlv@gmail.com>
2020-09-25 20:31:06 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Routes,RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SigninComponent } from './signin/signin.component';
+import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatSelectModule} from '@angular/material/select';
+import { MatNativeDateModule} from '@angular/material/core';
+
+
+
+
+const routes: Routes = [
+  {path:"signin", component:SigninComponent},
+  {path:"registration", component:RegistrationComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegistrationComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule, 
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatRadioModule, 
+    MatSelectModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [ MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
